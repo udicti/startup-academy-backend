96dada8b11738f340f16f52104e2dd8ce8b82e79
horaceTorvalds <fredy.masika@gmail.com>
2021-04-25 15:38:49 +0300

    added udictihub.tk in whitelist the right way

--- udictihub/settings.py
+++ udictihub/settings.py
@@ -81,12 +81,12 @@ DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
-ALLOWED_HOSTS = ["http://127.0.0.1:4200",'udictihub.tk']
+ALLOWED_HOSTS = ["http://127.0.0.1:4200",'https://udictihub.tk']
 
 CORS_ALLOWED_ORIGINS = [
        'http://localhost:4200',
        'http://127.0.0.1:4200',
-       'udictihub.tk'
+       'https://udictihub.tk'
 ]
 
 # Password validation
