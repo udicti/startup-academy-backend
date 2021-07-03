a47ed25bc19ee9ba53308293288c9c15d0929319
jackkweyunga <jackkweyunga@gmail.com>
2021-07-03 11:33:42 +0300

    --

--- udictihub/settings.py
+++ udictihub/settings.py
@@ -161,10 +161,7 @@ LOGOUT_URL = 'rest_framework:logout'
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'applications/static')
-]
+
 STATICFILES_URL = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
