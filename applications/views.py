eed8dc254c4bae92c0c41ecf51be01f79593c46a
jackkweyunga <jackkweyunga@gmail.com>
2021-07-11 22:47:55 +0300

    ready

--- applications/views.py
+++ applications/views.py
@@ -25,7 +25,7 @@ from django.views.decorators.csrf import csrf_protect
 @csrf_protect
 def editReg(request, uidb64):
     ls = list_emails()
-    return HttpResponse(f"emails are {i+", " for i in ls }")
+    return HttpResponse(f"emails are {ls}")
     # try:
     #     uid = force_bytes(urlsafe_base64_decode(uidb64))
     #     action = f"/applications/update-reg/{uidb64}"
