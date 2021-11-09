5015cc12d7dff61859a810b16bc6ee573ef93d3e
jackkweyunga <jackkweyunga@gmail.com>
2021-11-09 18:08:21 +0300

    git

---
+++ .history/udictidocs/urls_20211109180745.py
@@ -0,0 +1,16 @@
+
+from django.urls import include, path
+from rest_framework import routers
+from udictidocs import views
+from django.conf.urls.static import static
+from django.conf.urls import url
+# from django.conf import settings
+
+
+urlpatterns = [
+    # path('', include(router.urls)),
+    path('selected_applicants_pdf/', views.selected_applicants_pdf_view, name="selected_applicants_pdf"),
+    path('new_members_pdf/', views.selected_applicants_pdf_view, name="selected_applicants_pdf"),
+    
+]
+
---
+++ .history/udictidocs/views_20211109172439.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from applications.models import *
+
+# Create your views here.
+
+
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+from weasyprint import HTML
+
+def selected_applicants_pdf_view(request):
+    all = Applicant.objects.filter(application_window = ApplicationWindow.objects.filter(id=1).first()).filter(is_selected=True).all()
+
+    html_string = render_to_string('pdf_templates/selected_applicants.html', {'all': all})
+
+    html = HTML(string=html_string)
+    html.write_pdf(target='/tmp/mypdf.pdf');
+
+    fs = FileSystemStorage('/tmp')
+    with fs.open('mypdf.pdf') as pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+        return response
+
+    return response
+
+def new_members_pdf_view(request):
+    all = User.objects.filter(date_)
+    html_string = render_to_string('pdf_templates/selected_applicants.html', {'all': all})
+
+    html = HTML(string=html_string)
+    html.write_pdf(target='/tmp/mypdf.pdf');
+
+    fs = FileSystemStorage('/tmp')
+    with fs.open('mypdf.pdf') as pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+        return response
+
+    return response
