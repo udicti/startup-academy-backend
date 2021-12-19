a89ddce610572f1fbcaa238e663a9e8a18c0ad7b
jackkweyunga <jackkweyunga@gmail.com>
2021-12-20 01:01:37 +0300

    finished adding a reset password views

---
+++ .history/dashboard/views_20211220000313.py
@@ -0,0 +1,607 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+from rest_framework import views
+
+from django.contrib.auth.models import User
+from django.contrib import messages
+from api.models import Attendance, Event, Project, AttendanceCode, AttendanceList, Teams, Mail
+from django.contrib.auth.decorators import user_passes_test
+
+from api.token_generator import account_activation_token
+from api.send_mail import send_mail
+from django.template.loader import render_to_string
+
+from django.contrib.sites.models import Site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+
+
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def dashboard(request):
+    
+    """This view is the home view of the dashboard
+
+    Returns:
+        [a html page]: [dashboard/dashboard.html]
+    """
+    
+    # hub members
+    members = User.objects.all()
+    
+    context = {
+        "title":"UDICTIHUB",
+        "members": members,
+        "active_members":len([i for i in User.objects.all() if i.is_active == True]),
+        "inactive_members": len([i for i in User.objects.all() if i.is_active == False]),
+        "active_projects":len([i for i in Project.objects.all()])
+    }
+    
+    return render(request, 'dashboard/dashboard.html', context=context)
+
+from django.urls import reverse
+    
+def send_activation_link(id):
+    
+    user = User.objects.filter(id=id).first()
+    
+    data = {}
+    
+    current_site = Site.objects.get_current()
+
+    email_subject = 'Activate Your Account'
+    
+    activation_url = reverse(
+        'activate', 
+        args=[urlsafe_base64_encode(force_bytes(user.pk)), account_activation_token.make_token(user)]
+    )
+    
+    data['email-body'] = [
+        f"p> Dear {user.username}",
+        f"p> We understand you faced some challenges during registrations, APOLOGY for that. "
+        f"p> Thank you for creating an account in Udictihub, Click the link below to activate your account.",
+        f"a> Follow this Activation Link href> {current_site.domain}{activation_url}",
+        f"p> After activating your account, you will be able to login.",
+    ]
+    
+    data["email-subject"] = email_subject
+    data["email-receiver"] = user.email
+    
+    # print(data)
+    
+    return send_mail(data)
+    
+   
+
+
+    
+# send account activation email to multiple users
+
+def send_activation_email_to_inactive_user(request, id):
+    
+    one = [i for i in User.objects.all() if i.is_active == False and i.id == id][0]
+    
+    if one is not None:
+        
+        # send activation email
+        send_activation_link(one.id)
+    
+    return redirect("member-view", id)
+
+
+
+# send account activation email to a single user 
+
+def send_activation_email_to_all_inactive_users(request):
+    
+    all = [i for i in User.objects.all() if i.is_active == False]
+    
+    for i in all:
+        
+        if i is not None:
+            
+            # send activation email
+            send_activation_link(i.id)
+            
+    
+    return redirect("dashboard")
+
+
+
+# password issues
+
+class ForgotPasswordView(View):
+    
+    class InputForm(forms.Form):
+        email = forms.EmailField(validators=[])
+
+    def get(self, request):
+        return render(request, "auth/forgot-password.html", context={'sent':False, 'email':''})
+
+    def post(self, request):
+
+        error = None
+
+        form = self.InputForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            user = user_get_by_email(email)
+            
+            if user:
+                send_email_for_password_reset(user, redirect_url=f"{SITE}/login/")
+                return render(request, "auth/forgot-password.html", context={'sent':True, 'email':email})
+            else:
+                error = "no user, with such email"
+        else:
+            error = form.errors.as_text()
+        return render(request, "auth/forgot-password.html", context={'sent':False, 'email':'', "error":error})
+        
+# add new password
+
+class AddNewPassword(View):
+    class InputForm(forms.Form):
+        email = forms.EmailField()
+        password = forms.CharField()
+        verify_password = forms.CharField()
+
+    def get(self, request, token, *args, **kwargs):
+        valid, user = default_token_generator.check_token(token)
+        if user:
+            return render(request, "auth/new-password.html", context={"user":user, "error":None})
+        # if not send to the resend link page
+        else:
+            return render(request, "auth/forgot-password.html", context={'sent':False, 'email':'', "error":"Link has expired, submit your email to send the link again "})
+
+
+    def post(self, request):
+        form = self.InputForm(request.POST)
+
+        user = {
+            "email": form.data['email']
+        }
+        
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+
+            user = user_get_by_email(email)
+            user.set_password(password)
+            user.save()
+
+            return redirect(f"{SITE}/login/")
+
+        else:
+            return render(request, "auth/new-password.html", context={"user":user, "error":form.errors.as_text()})
+
+
+
+def member_view(request, id):
+    
+    """Members View"""    
+    
+    context = {
+        "member": User.objects.filter(id=id).first()
+    }
+    
+    return render(request, 'dashboard/member_view.html', context=context)
+
+
+def deactivate_member(request, id):
+    """ Activate member """
+    
+    member = User.objects.filter(id=id).first()
+    if member:
+        try:
+            member.is_active = False
+            member.save()
+            messages.success(request, 'member deactivated successfully')
+        except:
+            messages.error(request, 'failed to deactivated member')
+    else:
+        messages.error(request, 'member not found')
+    
+    return redirect('member-view', id=id)
+
+
+def activate_member(request, id):
+    """ Activate member """
+    
+    member = User.objects.filter(id=id).first()
+    if member:
+        try:
+            member.is_active = True
+            member.save()
+            messages.success(request, 'member activated successfully')
+        except:
+            messages.error(request, 'failed to activated member')
+    else:
+        messages.error(request, 'member not found')
+    
+    return redirect('member-view', id=id)
+
+
+def delete_member(request, id):
+    """ Delete member """
+    
+    member = User.objects.filter(id=id).first()
+    if member:
+        try:
+            member.delete()
+            messages.success(request, 'member deleted successfully')
+        except:
+            messages.error(request, 'failed to delete member')
+    else:
+        messages.error(request, 'member not found')
+    
+    return redirect('dashboard')
+
+
+
+def project_view(request, id):
+    
+    """Project View"""
+    
+    context = {
+        "project": Project.objects.filter(id=id).first()
+    }
+
+    return render(request, 'dashboard/project_view.html', context=context)
+
+
+class Login(View):
+    
+    """Login View 
+    """
+    
+    def get(self, request):
+        
+        context = {
+            
+        }
+        
+        return render(request, 'dashboard/login.html', context=context)
+
+    def post(self, request):
+        name = request.POST["name"]
+        password = request.POST["password"]
+
+        _user = User.objects.filter(username=name).first()
+
+        if _user is not None:
+            username = _user.username
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None and user.is_superuser:
+                login(request, user)
+
+                messages.success(request,  f"Welcome {request.user}")
+                return redirect('dashboard')
+            elif user is not None and user.is_superuser == False:
+                login(request, user)
+
+                messages.success(request,  f"Welcome {request.user} to your attendance view")
+                return redirect('member_attendance_view')
+            
+
+        messages.error(request,  "wrong credentials")
+        return redirect('login')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+def projects_view(request):
+    
+    """Projects View
+    """
+    
+    context = {
+        
+    }
+    
+    return render(request, 'dashboard/projects.html', context=context)
+
+
+def attendance_view(request):
+    
+    """
+    Attendance View
+    """ 
+    
+    context = {
+        "attendance_codes": AttendanceCode.objects.all(),
+        "attendances": Attendance.objects.all(),
+        "events": Event.objects.all()
+    }
+    
+    return render(request, 'dashboard/attendance.html', context=context)
+
+
+def attendance_details_view(request, id):
+    
+    """Attendance Details View
+    """ 
+    
+    context = {
+        "attendance": Attendance.objects.filter(id=id).first()
+    }
+    
+    return render(request, 'dashboard/attendance_details.html', context=context)
+
+
+
+class EventsView(View):
+    
+    def get(self, request, id):
+        event = Event.objects.filter(id=id).first()
+        context = {
+            "event": event,
+            "attendance_codes": AttendanceCode.objects.filter(event=event).all()
+        }
+        return render(request, "dashboard/event.html", context)
+    
+    def post(self, request):
+        
+        name = request.POST["name"]
+        description = request.POST["description"]
+        
+        event = Event(name=name, description=description)
+        event.save()
+        messages.success(request, "Created new event")
+        return render("attendance")
+
+from datetime import date
+class MemberAttendanceView(View):
+    
+    """Mamber View """
+    
+    def get(self, request):
+        day = Attendance.objects.filter(date=date.today()).first()
+        attended = None
+        if day:
+            attended = AttendanceList.objects.filter(attendant=request.user, day=day).first()
+        
+        context = {
+            "member": request.user,
+            "today": date.today(),
+            "attended": attended,
+            "attendance": AttendanceList.objects.filter(attendant=request.user).all()
+        }
+        
+        return render(request, 'dashboard/member_attendance_view.html', context=context) 
+        
+    def post(self, request):
+        day = Attendance.objects.filter(date=date.today()).first()
+        
+        attended = AttendanceList.objects.filter(attendant=request.user, day=day).first()
+                
+        if attended:
+            messages.error(request, "You already signed in.")
+            return redirect('member_attendance_view')
+            
+        code = request.POST["code"]
+        atc = AttendanceCode.objects.filter(user = request.user, code=code).first()
+        
+        if atc is None:
+            messages.error(request, "The code is not valid")
+            return redirect('member_attendance_view')
+        
+        new = AttendanceList(attendant=request.user, day=day)
+        new.save()
+        
+        messages.success(request, "Welcome to Udictihub, You signed your attendance successfully.")
+        return redirect('member_attendance_view')
+
+
+def start_attendence(request, event_id):
+    
+    event = Event.objects.filter(id=event_id).first()
+    
+    if event is None:
+        messages.success(request, 'No such event')
+        return redirect('attendance')
+    
+    description = request.POST.get('description')
+    
+    all = User.objects.all()
+    
+    new = Attendance(date=date.today())
+    new.event = event
+    new.description = description
+    new.save()
+    
+    if AttendanceCode.objects.filter(event=event).first() is not None:
+        AttendanceCode.objects.filter(event=event).delete()
+
+    for i in all:
+        code = AttendanceCode(user=i)
+        code.event = event
+        code.save()
+    messages.success(request, 'attendance created successfully')
+    return redirect('attendance')
+
+
+# EventsView
+
+
+class teams_view(View):
+    
+    """teams View
+    """
+    
+    def get(self,  request):
+        context = {
+            "teams": Teams.objects.all()
+        }
+        
+        return render(request, 'dashboard/teams.html', context=context)
+    
+    def post(self, request):
+        # creating a new team
+        
+        return redirect('teams')
+
+# team actions
+def team_view(request, id):
+    team = Teams.objects.filter(id=id).first()
+    context = {
+        "team": team,
+        "members": [ i for i in team.members.all()],
+        "per_progress": 60,
+        "nmembers": team.members.all().count()
+    }
+    
+    return render(request, 'dashboard/team.html', context)
+
+def team_delete(request, id):
+    
+    context = {
+        
+    }
+    
+    return redirect('team-view')
+
+def team_deactivate(request, id):
+    
+    context = {
+        
+    }
+    
+    return redirect('team-view')
+
+def team_member_add(request, user_id):
+    
+    context = {
+        
+    }
+    
+    return redirect('team-view')
+
+def team_member_remove(request, user_id):
+    
+    context = {
+        
+    }
+    
+    return redirect('team-view')
+
+
+
+def documents_view(request):
+    
+    """Documents View
+    """
+    
+    context = {
+        
+    }
+    
+    return render(request, 'dashboard/documents.html', context=context)
+
+
+def blogs_view(request):
+    
+    """Blogs View
+    """
+    
+    context = {
+        
+    }
+    
+    return render(request, 'dashboard/blog.html', context=context)
+
+
+def mails_view(request):
+    
+    """Emails View
+    """
+    
+    context = {
+        "emails": Mail.objects.all()
+    }
+    
+    return render(request, 'dashboard/mails.html', context=context)
+
+
+def settings_view(request):
+    
+    """Settings View
+    """
+    
+    context = {
+    }
+    
+    return render(request, 'dashboard/settings.html', context=context)
+
+
+def applications_view(request):
+    
+    """Applications View
+    """
+    
+    context = {
+    }
+    
+    return render(request, 'dashboard/applications.html', context=context)
+
+
+# actions
+
+def deactivate_user(id):
+    user = User.objects.filter(id=id).firts()
+    
+    if user is not None:
+        
+        user.is_active = False
+        user.save()
+        
+        return True
+    return False
+    
+def activate_user(id):
+    
+    user = User.objects.filter(id=id).firts()
+    
+    if user is not None:
+        
+        user.is_active = True
+        user.save()
+        
+        return True
+    return False
+
+    
+def deactivate_user_view_via_dashboard(request, id):
+    
+    """ A view to deactivate a member via the dashboard """
+    
+    if deactivate_user(id):
+        messages.success(request, "the user was deactivated successfully")
+    else:
+        messages.error(request, "Failed to deactivate user")
+    return redirect('dashboard')
+
+
+
+def member_activity(id):
+    
+    """A function to collect members activities
+    
+        returns
+        - projects
+        - posts
+        - comments
+        - likes
+        - teams
+        
+    """
+    
+    activity = {}
+    
+    return activity
---
+++ dashboard/token.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+
+from udictihub import settings
+from django.contrib.auth import get_user_model
+from django.utils.crypto import constant_time_compare, salted_hmac
+from django.utils.http import base36_to_int, int_to_base36, urlsafe_base64_decode, urlsafe_base64_encode
+
+
+class EmailVerificationTokenGenerator:
+    """
+    Strategy object used to generate and check tokens for the password
+    reset mechanism.
+    """
+    try:
+        key_salt = settings.CUSTOM_SALT
+    except AttributeError:
+        key_salt = "django-email-verification.token"
+    algorithm = None
+    secret = settings.SECRET_KEY
+
+    def make_token(self, user, expiry=None):
+        """
+        Return a token that can be used once to do a password reset
+        for the given user.
+        Args:
+            user (Model): the user
+            expiry (datetime): optional forced expiry date
+        Returns:
+             (tuple): tuple containing:
+                token (str): the token
+                expiry (datetime): the expiry datetime
+        """
+        if expiry is None:
+            return self._make_token_with_timestamp(user, self._num_seconds(self._now()))
+        return self._make_token_with_timestamp(user, self._num_seconds(expiry) - settings.EMAIL_TOKEN_LIFE)
+
+    def check_token(self, token):
+        """
+        Check that a password reset token is correct.
+        Args:
+            token (str): the token from the url
+        Returns:
+            (tuple): tuple containing:
+                valid (bool): True if the token is valid
+                user (Model): the user model if the token is valid
+        """
+
+        try:
+            email_b64, ts_b36, _ = token.split("-")
+            email = urlsafe_base64_decode(email_b64).decode()
+            if hasattr(settings, 'EMAIL_MULTI_USER') and settings.EMAIL_MULTI_USER:
+                users = get_user_model().objects.filter(email=email)
+            else:
+                users = [get_user_model().objects.get(email=email)]
+            ts = base36_to_int(ts_b36)
+        except (ValueError, get_user_model().DoesNotExist):
+            return False, None
+
+        user = next(filter(lambda u: constant_time_compare(self._make_token_with_timestamp(u, ts)[0], token), users),
+                    None)
+
+        if not user:
+            return False, None
+
+        now = self._now()
+        if (self._num_seconds(now) - ts) > settings.EMAIL_TOKEN_LIFE:
+            return False, None
+
+        return True, user
+
+    def _make_token_with_timestamp(self, user, timestamp):
+        email_b64 = urlsafe_base64_encode(user.email.encode())
+        ts_b36 = int_to_base36(timestamp)
+        hash_string = salted_hmac(
+            self.key_salt,
+            self._make_hash_value(user, timestamp),
+            secret=self.secret,
+        ).hexdigest()
+        # print(hash_string)
+        return f'{email_b64}-{ts_b36}-{hash_string}', \
+               datetime.fromtimestamp(timestamp + settings.EMAIL_TOKEN_LIFE)
+
+    @staticmethod
+    def _make_hash_value(user, timestamp):
+        login_timestamp = '' if user.last_login is None else user.last_login.replace(microsecond=0, tzinfo=None)
+        return str(user.pk) + user.password + str(login_timestamp) + str(timestamp)
+
+    @staticmethod
+    def _num_seconds(dt):
+        return int((dt - datetime(2001, 1, 1)).total_seconds())
+
+    @staticmethod
+    def _now():
+        return datetime.now()
+
+
+default_token_generator = EmailVerificationTokenGenerator()
